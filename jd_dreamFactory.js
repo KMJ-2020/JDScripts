d73134739ba923bafac039a54dcd9a3c6a8aefe5
Mr. G <mr.g@MrdeMac-mini.local>
2021-10-23 21:30:52 +0800

    aaa

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -43,10 +43,10 @@ let tuanActiveId = ``, hasSend = false;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
 const inviteCodes = [
-  'FqJ0wZsdeSBIU1jdpsdsSQ==@f9pwpYKgC564wzJ7JLlWuQ==@50pOf2wjgupGYI3qIgqAAQ==',
-  "FqJ0wZsdeSBIU1jdpsdsSQ==@f9pwpYKgC564wzJ7JLlWuQ==@50pOf2wjgupGYI3qIgqAAQ==",
-  'FqJ0wZsdeSBIU1jdpsdsSQ==@f9pwpYKgC564wzJ7JLlWuQ==@50pOf2wjgupGYI3qIgqAAQ==',
-  'FqJ0wZsdeSBIU1jdpsdsSQ==@f9pwpYKgC564wzJ7JLlWuQ==@50pOf2wjgupGYI3qIgqAAQ=='
+  'FqJ0wZsdeSBIU1jdpsdsSQ==@f9pwpYKgC564wzJ7JLlWuQ==@X0c9jSsRJlGhrxZjnBHUWA==',
+  "FqJ0wZsdeSBIU1jdpsdsSQ==@f9pwpYKgC564wzJ7JLlWuQ==@X0c9jSsRJlGhrxZjnBHUWA==",
+  'FqJ0wZsdeSBIU1jdpsdsSQ==@f9pwpYKgC564wzJ7JLlWuQ==@X0c9jSsRJlGhrxZjnBHUWA==',
+  'FqJ0wZsdeSBIU1jdpsdsSQ==@f9pwpYKgC564wzJ7JLlWuQ==@X0c9jSsRJlGhrxZjnBHUWA=='
 ];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 $.tuanIds = [];
